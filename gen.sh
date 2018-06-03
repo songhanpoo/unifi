9711ee093ba66f5da66d265ad874649a505f9520
songhanpoo <33808968+songhanpoo@users.noreply.github.com>
2018-06-04 05:15:41 +0700

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,53 +1,49 @@
 #!/bin/sh
 
-sitename=(site1 site2)
 # Files needed
-#pwd=`pwd`
-#. $pwd/unifi-api.sh
-
-for i in ${sitename[@]};do
 pwd=`pwd`
-. $pwd/${i}.sh
+. $pwd/unifi-api.sh
 
 # Generation settings
 time=60 # Voucher time limit (minutes)
 amount=3000 # New vouchers to generate
-
+note=(hieutaocode)
 # HTML Settings
 line1="WiFi Voucher"
 line2="Valid for 60 minutes"
-author="make by Hieu Hien Hoa"
+
 # Generate vouchers
 unifi_login
-voucherID=`unifi_create_voucher $time $amount $note`
-unifi_get_vouchers $voucherID > vouchers.tmp
+unifi_create_voucher $time $amount $note
+unifi_get_vouchers > vouchers.tmp
+unifi_logout
+vouchers=`awk -F "[,:]" '{for(i=1;i<=NF;i++){if($i~/time\042/){print $(i+1)} } }' vouchers.tmp | sed 's/\"//g'`
+
+for code in $vouchers
+do
+    line3=${code:0:24}
+        create_time1=$line3
+        echo " $create_time1" >> time.tmp
+
+done
+getvoucher=`awk '!a[$0]++' time.tmp`
+echo " $getvoucher" >> final.txt
+
+create_time=`head -n 1 final.txt`
+echo $create_time
+unifi_login
+unifi_get_vouchers $create_time > final.tmp
 unifi_logout
 
-vouchers=`awk -F"[,:]" '{for(i=1;i<=NF;i++){if($i~/code\042/){print $(i+1)} } }' vouchers.tmp | sed 's/\"//g'`
 
-# Build HTML
-#if [ -e vouchers.html ]; then
-#  echo "Removing old vouchers."
-#  rm vouchers.html
-#fi
+vouchers=`awk -F"[,:]" '{for(i=1;i<=NF;i++){if($i~/code\042/){print $(i+1)} } }' final.tmp | sed 's/\"//g'`
 
-echo '<html><head><link rel="stylesheet" href="style.css" /></head><body>' >> ${i}.html
+
+echo '<html><head><link rel="stylesheet" href="style.css" /></head><body>' >> vouchers.html
 
 for code in $vouchers
 do
     line3=${code:0:5}" "${code:5:10}
-    html='<div class="voucher"><div class="line1">'$line1'</div><div class="line2">'$line2'</div><div class="line3">'$line3'</div><div class="author">'$author'</div></div>'
-    echo $html >> ${i}.html
+    html='<div class="voucher"><div class="line1">'$line1'</div><div class="line2">'$line2'</div><div class="line3">'$line3'</div></div>'
+    echo $html >> vouchers.html
 done
-
-echo "</body></html>" >> ${i}.html
-done
-# Remove tmp
-#if [ -e vouchers.tmp ]; then
-#  echo "Removing vouchers tmp file."
-#  rm vouchers.tmp
-#fi
-cd /mnt
-rm -rf *.html
-cd /root/unifi-voucher-generator
-cp -rf *.html /mnt/
