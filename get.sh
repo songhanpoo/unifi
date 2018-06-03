b0b8c47745936cdbcbdebc7582a0e715cfa2938d
songhanpoo <33808968+songhanpoo@users.noreply.github.com>
2018-06-04 04:52:06 +0700

    Create get.sh

---
+++ get.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Files needed
+pwd=`pwd`
+. $pwd/unifi-api.sh
+
+
+
+vouchers=`awk -F "[,:]" '{for(i=1;i<=NF;i++){if($i~/time\042/){print $(i+1)} } }'$
+
+for code in $vouchers
+do
+    line3=${code:0:24}
+        create_time=$line3
+        echo " $create_time" >> time.tmp
+
+done
